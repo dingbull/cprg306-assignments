22c56a7cd88a4f27c00c3541099818f1cd75a2ce
Rolan Ho <116419895+dingbull@users.noreply.github.com>
2023-11-06 22:14:32 -0700

    Added quotes for firebaseConfig

--- app/week8/page.js
+++ app/week8/page.js
@@ -1,31 +1,25 @@
 "use client";
-
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { useUserAuth } from './_utils/auth-context'; // Adjust the path to where your auth-context is located
-
+ 
+import React, { useEffect } from "react";
+// import { useRouter } from 'next/router';
+import { useUserAuth } from "./_utils/auth-context"; // Adjust the path to where your auth-context is located
+ 
 export default function Page() {
-  const { user } = useUserAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    // If there's no user, redirect to the login page
-    if (!user) {
-      router.push('/login'); // Adjust this to the route of your login page
-    }
-  }, [user, router]);
-
-  // Render a loading state or nothing until the user is verified
-  if (!user) {
-    return <p>Loading...</p>; // or return null to render nothing
-  }
-
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+ 
+  const handleSignIn = async () => {
+    await gitHubSignIn();
+  };
+ 
   // Your page content goes here, wrapped in a conditional that checks for the user
   return (
     <main>
       {/* Your page content that should be protected */}
-      <p>Welcome, {user.displayName}!</p>
+      <p>Welcome, {user?.displayName}!</p>
+ 
+      <button onClick={handleSignIn}>Sign in with GitHub</button>
       {/* Rest of your page components */}
     </main>
   );
 }
+ 
